e90dbe06da4d9c53a47c3e12c0f0daedd23bbdde
Lanh Nguyen <nguyenkimlanh2010@gmail.com>
2025-08-22 12:21:51 +0700

    Run migrations before starting local scripts

--- scripts/run-analyzer.sh
+++ scripts/run-analyzer.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 # Run the analyzer once to fetch analysis for all tickers
 set -e
+set -a
+. ./.env
+set +a
+migrate -path migrations -database "$DATABASE_URL" up
 GO111MODULE=on go run ./cmd/analyzer
